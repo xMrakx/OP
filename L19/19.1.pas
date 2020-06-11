a038c987d6de748c69e730fabad38dd9d65f3be9
xMrakx <61603179+xMrakx@users.noreply.github.com>
2020-06-11 03:31:53 +0300

    Add files via upload

---
+++ L19/19.1.pas
@@ -0,0 +1,34 @@
+PROGRAM Prime(INPUT, OUTPUT);
+CONST
+  Max = 11;
+  Min = 2;
+VAR
+  I, D: INTEGER;
+  IsPrime: BOOLEAN;
+  Sieve: SET OF 0 .. Max;
+BEGIN {Prime}
+  Sieve := [Min .. Max];
+  I := Min;
+  WHILE (I <= Max)
+  DO
+    BEGIN
+      D := I;
+      WRITELN('Min - ', I);
+      IF (D IN Sieve)
+      THEN
+        BEGIN
+          WRITE('Deleted - ');
+          WHILE (D <= Max)
+          DO
+            BEGIN
+              Sieve := Sieve - [D];
+              WRITE(D, ' ');
+              D := D + I
+            END;
+          WRITELN
+        END;
+      I := I + 1      
+    END;
+  WRITELN       
+END. {Prime}  
+  
