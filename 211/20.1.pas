b8bf83d3f68a85ff10a92a24f78ff86cfdd7d8ea
xMrakx <61603179+xMrakx@users.noreply.github.com>
2020-07-08 01:51:46 +0300

    Add files via upload

---
+++ 211/20.1.pas
@@ -0,0 +1,82 @@
+PROGRAM Encryption(INPUT, OUTPUT);
+{Переводит символы из INPUT в код согласно Chiper 
+  и печатает новые символы в OUTPUT}
+CONST
+  Len = 20;
+TYPE
+  Str = ARRAY [1 .. Len] OF 'A' .. 'Z';
+  Chiper = ARRAY ['A' .. 'Z'] OF CHAR;
+  Length = 0 .. Len;
+VAR
+  Msg: Str;
+  Code: Chiper;
+  I: Length;
+ 
+PROCEDURE Initialize(VAR Code: Chiper);
+{Присвоить Code шифр замены}
+BEGIN {Initialize}
+  Code['A'] := 'Z';
+  Code['B'] := 'Y';
+  Code['C'] := 'X';
+  Code['D'] := '#';
+  Code['E'] := 'V';
+  Code['F'] := 'U';
+  Code['G'] := 'T';
+  Code['H'] := 'S';
+  Code['I'] := 'I';
+  Code['J'] := 'Q';
+  Code['K'] := 'P';
+  Code['L'] := '!';
+  Code['M'] := 'N';
+  Code['N'] := 'M';
+  Code['O'] := '2';
+  Code['P'] := 'K';
+  Code['Q'] := '$';
+  Code['R'] := 'D';
+  Code['S'] := 'H';
+  Code['T'] := '*';
+  Code['U'] := 'F';
+  Code['V'] := 'E';
+  Code['W'] := 'T';
+  Code['X'] := 'C';
+  Code['Y'] := 'B';
+  Code['Z'] := 'A';
+END;  {Initialize}
+ 
+PROCEDURE Encode(VAR S: Str);
+{Выводит символы из Code, соответствующие символам из S}
+VAR
+  Index: 1 .. Len;
+BEGIN {Encode}
+  FOR Index := 1 TO I
+  DO
+    IF S[Index] IN ['A' .. 'Z']
+    THEN
+      WRITE(Code[S[Index]])
+    ELSE
+      WRITE(S[Index]);
+  WRITELN
+END;  {Encode}
+ 
+BEGIN {Encryption}
+  {Инициализировать Code}
+  Initialize(Code);
+  WHILE NOT EOF
+  DO
+    BEGIN
+      {читать строку в Msg и распечатать ее}
+      I := 0;
+      WHILE NOT EOLN AND (I < Len)
+      DO
+        BEGIN
+          I := I + 1;
+          READ(Msg[I]);
+          WRITE(Msg[I])
+        END;
+      READLN;
+      WRITELN;
+     {распечатать кодированное сообщение}
+      Encode(Msg)
+    END
+END.  {Encryption}
+
