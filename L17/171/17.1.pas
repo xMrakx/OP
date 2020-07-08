3bcf13ed468e8e11a5dfaac490d5fe045b67040f
xMrakx <61603179+xMrakx@users.noreply.github.com>
2020-07-08 23:15:31 +0300

    Add files via upload

--- L17/171/17.1.pas
+++ L17/171/17.1.pas
@@ -5,25 +5,31 @@ VAR
 BEGIN {ReadDigit}
   IF NOT (EOLN)
   THEN
-    READ(F, Ch);
-  IF Ch = '0' THEN D := 0 ELSE
-  IF Ch = '1' THEN D := 1 ELSE
-  IF Ch = '2' THEN D := 2 ELSE
-  IF Ch = '3' THEN D := 3 ELSE
-  IF Ch = '4' THEN D := 4 ELSE
-  IF Ch = '5' THEN D := 5 ELSE
-  IF Ch = '6' THEN D := 6 ELSE
-  IF Ch = '7' THEN D := 7 ELSE
-  IF Ch = '8' THEN D := 8 ELSE
-  IF Ch = '9' THEN D := 9
-    ELSE
-      D := -1
+    BEGIN
+      READ(F, Ch);
+      IF Ch = '0' THEN D := 0 ELSE
+      IF Ch = '1' THEN D := 1 ELSE
+      IF Ch = '2' THEN D := 2 ELSE
+      IF Ch = '3' THEN D := 3 ELSE
+      IF Ch = '4' THEN D := 4 ELSE
+      IF Ch = '5' THEN D := 5 ELSE
+      IF Ch = '6' THEN D := 6 ELSE
+      IF Ch = '7' THEN D := 7 ELSE
+      IF Ch = '8' THEN D := 8 ELSE
+      IF Ch = '9' THEN D := 9
+        ELSE
+          D := -1
+    END
+  ELSE
+    D := -1        
 END;  {ReadDigit}
 VAR
   CurNum, Sum: INTEGER;
+  Found: BOOLEAN;
 BEGIN {Sum}
   Sum := 0;
   CurNum := 0;
+  Found := NOT EOLN;
   WHILE (CurNum <> -1)
   DO
     BEGIN
@@ -32,5 +38,5 @@ BEGIN {Sum}
       THEN
         Sum := Sum + CurNum
     END;
-  WRITELN('Ã‘Ã³Ã¬Ã¬Ã  Ã¶Ã¨Ã´Ã° Ã°Ã Ã¢Ã­Ã  ', Sum:2) 
+  WRITELN('Ñóììà öèôð ðàâíà ', Sum:2) 
 END. {Sum}
